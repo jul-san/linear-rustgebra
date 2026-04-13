833230bf89ae7894cafd4c369a4b7308087dadef
Julian Sanchez <julianwsan@gmail.com>
2026-04-12 21:27:29 -0400

    removed comments containing old code and added proper testing print statements

--- src/main.rs
+++ src/main.rs
@@ -3,99 +3,28 @@ mod operations;
 
 use std::io::{self};
 use matrix::Matrix;
-use operations::row_ops;
+use operations::{row_ops, echelon};
 
 fn main() {
     let mut m = Matrix::new(vec![
         vec![1.0, 2.0, 3.0], 
-        vec![4.0, 5.0, 6.0],
-        vec![7.0, 8.0, 9.0]]);
+        vec![2.0, 4.0, 6.0],
+        vec![1.0, 1.0, 1.0]]);
 
+    print_section("Printing Matrix");
     m.print_matrix();
 
+    /*
+    print_section("Swapping Rows 0 and 2");
     row_ops::swap_row(&mut m, 0, 2);
+    m.print_matrix();
+    */ 
 
+    print_section("Echelon Form");
+    echelon::row_echelon(&mut m);
     m.print_matrix();
 }
 
-/*
-fn row_echelon(mut matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-  let row_count = matrix.len();
-  let col_count = matrix[0].len();
-
-  let mut pivot_row = 0;
-
-  for pivot_col in 0..col_count {
-    if pivot_row >= row_count {
-      break;
-    }
-
-    // finding pivot
-    let mut max_row = pivot_row;
-    while max_row < row_count && matrix[max_row][pivot_col] == 0.0 {
-      max_row += 1;
-    }
-
-    if max_row == row_count {
-      continue;
-    }
-
-    // swap rows
-    if max_row != pivot_row {
-      matrix.swap(pivot_row, max_row);
-    }
-
-    // make pivot = 1
-    let pivot = matrix[pivot_row][pivot_col];
-    for col in pivot_col..col_count {
-      matrix[pivot_row][col] /= pivot;
-    }
-
-    // eliminate below
-    for row in pivot_row + 1..row_count {
-      let factor = matrix[row][pivot_col];
-
-      for col in pivot_col..col_count {
-        matrix[row][col] -= factor * matrix[pivot_row][col];
-      }
-    }
-
-    pivot_row += 1;
-  }
-
-  return matrix;
+fn print_section(title: &str) {
+  println!("\n==================== {} ====================\n", title);
 }
-
-fn define_values(mut matrix: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-
-    let mut row_counter = 0;
-    for row in 0..matrix.len() {
-        loop{
-            println!("Enter values for the row {row_counter} seperated by spaces:\n");
-        
-            let mut input = String::new();
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Invalid values");
-
-            let values: Vec<f64> = input
-                .split_whitespace()
-                .map(|x| x.parse::<f64>().expect("Enter a valid input"))
-                .collect();
-
-            if values.len() != matrix[row].len() {
-                let length = matrix[row].len();
-                println!("Please enter the correct number of values ({length})\n");
-                continue;
-            }
-        
-            matrix[row] = values;
-            row_counter += 1;
-            println!("");
-            break;
-        }
-    }
-
-    return matrix;
-}
-*/
